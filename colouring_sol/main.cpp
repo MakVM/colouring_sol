ca875988ae73f6f6bb8c9d8b51aa24139542f266
MakVM <verunchikmakarova2011@gmail.com>
2022-05-08 17:30:38 +0300

    d

--- colouring_sol/main.cpp
+++ colouring_sol/main.cpp
@@ -10,11 +10,40 @@
 #include <string>
 #include <fstream>
 using namespace std;
+#include <set>
+#include <tuple>
+#include <vector>
 
+//DSatur algorithm
+struct vertex_properites {
+    int saturation; // Saturation degree of the vertex
+    int degree; // Degree in the uncoloured subgraph
+    int index; // Index of vertex
+};
 
+struct maximising_saturation{
+    bool operator() (const vertex_properites& one, const vertex_properites& two) const
+    {
+        return tie(one.saturation, one.degree, one.index)
+        > tie(two.saturation, two.degree, two.index)
+    }
+};
 
 int main() {
 
+    int num_of_colours;
+    int V;
+    int E;
+    cin>> V;
+    cin>> E;
+    
+    
+    for(int i = 0; i<E;i++)
+    {
+        
+    }
+    
+    cout<<num_of_colours<<endl;
     
     
     return 0;
